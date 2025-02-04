e44ca8f87c72be8d1d4a2d6c95b2e12f82d3ed31
vpiszz <sosofiapopovaaktobe2006@gmail.com>
2025-02-04 21:01:13 +0500

    Create MainWindow.xaml
    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement el in GroupButton.Children)
+            {
+                if(el is Button)
+                {
+                    ((Button)el).Click += ButtonClick;
+                }
+            }
+
+      }
+    private void ButtonClick(Object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string textButton = ((Button)e.OriginalSource).Content.ToString();
+
+                if (textButton == "C")
+                {
+                    text.Clear();
+                }
+                else if (textButton == "x")
+                {
+                    text.Text = text.Text.Substring(text.Text.Length - 1);
+                }
+                else if (textButton == "=")
+                {
+                    text.Text = new System.Data.DataTable().Compute(text.Text, null).ToString();
+                }
+                else text.Text += textButton;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+    }
+}
